d3390020504081d3e66af0c3ec47e3d04d5a2aae
Lasarussp <001lasarus@gmail.com>
2023-03-22 19:58:17 +0530

    add

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+dotenv.config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 app.use(express.json());
 const dotenv = require('dotenv');
-dotenv.config();
 const cors = require("cors");
 app.use(cors());
 const jwt = require("jsonwebtoken");
